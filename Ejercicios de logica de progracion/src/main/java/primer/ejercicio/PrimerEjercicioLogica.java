5abd5e28938eb63ec699cad6dcf0c19faa2ddc14
Daniel1998-cenec <danielvh157@gmail.com>
2024-06-16 01:36:24 +0200

    Primer bloque de ejercicio listo

---
+++ Ejercicios de logica de progracion/src/main/java/primer/ejercicio/PrimerEjercicioLogica.java
@@ -0,0 +1,54 @@
+package primer.ejercicio;
+
+public class PrimerEjercicioLogica {
+
+	public static void main(String[] args) {
+		
+		// #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+		
+		// https://www.java.com/es/
+		
+		/*
+		 * comentario 
+		 * de
+		 * varias 
+		 * lineas
+		 * 
+		 * */
+		
+		String miPrimeraVariable="mi primera variable";
+		
+		final String variableNoModificable="Esta variable nose puede modificar";
+		
+		byte b=1;
+		
+		short s=20000;
+		
+		int i=2000000000;
+		
+		long l=2000000000000000000l;
+		
+		long maxLong= Long.MAX_VALUE;
+		
+		long minLong= Long.MIN_VALUE;
+		
+		float f=1.11f;
+		
+		double d=100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.0d;
+		
+		char a='a';
+		
+		boolean verdadero=true;
+		
+		var integer=100;
+	
+		String usuario="DanielCesar7";
+		
+		System.out.println("Este es el numero más grande que te puede dar la variable long " + maxLong+ "\n" + "\rEste es el numero mas pequeño que te puede dar la variable long " +minLong );
+		
+		System.out.println("Hola javi soy "+ usuario);
+		
+
+	}
+
+}
